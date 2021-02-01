4ac4570278e535488a18f962b040cb4cd480f0c3
Francisco Macedo <franciscomacedo@lisboaenova.org>
2021-02-01 17:22:34 +0000

    auth with frontend completly working

---
+++ frontend/src/views/Logout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="log">
+    ASDASDASDASD
+  </div>
+</template>
+
+<script>
+  export default {
+    created () {
+      this.$store.dispatch('logoutUser')
+        .then(() => {
+          this.$router.push({ name: 'home' })
+        })
+    }
+  }
+</script>
+
+<style scoped>
+</style>
